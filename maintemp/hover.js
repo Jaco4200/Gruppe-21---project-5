5522b241f259ba176ece1ad7e5c6d6b759301ac3
JETEXODUS <95678727+JETEXODUS@users.noreply.github.com>
2022-04-18 22:39:41 +0200

    opdateret nyhedshover

---
+++ maintemp/hover.js
@@ -0,0 +1,9 @@
+console.log("js is running :)");
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay", "forberedelsespopup", "ekstramaterialepopup").style.display = "none";
+}
