066b736aef182f25dd946ff5bddde0de16e94317
JETEXODUS <95678727+JETEXODUS@users.noreply.github.com>
2022-04-19 10:56:13 +0200

    Der er lavet en læs mere knap, som skifter farve når man hover musen over

--- maintemp/hover.js
+++ maintemp/hover.js
@@ -7,3 +7,14 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
+document.getElementById("readmore").onmouseover = function() {mouseOver()};
+document.getElementById("readmore").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("readmore").style.color = "purple";
+}
+
+function mouseOut() {
+  document.getElementById("readmore").style.color = "blue";
+}
