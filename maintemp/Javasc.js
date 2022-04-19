c9c16992a14cd52441c7af6569f7247a95558ebb
Jaco4200 <95678568+Jaco4200@users.noreply.github.com>
2022-04-19 13:11:32 +0200

    gmap

--- maintemp/Javasc.js
+++ maintemp/Javasc.js
@@ -10,5 +10,29 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3500); // Change image every 2 seconds
+  setTimeout(showSlides, 3500); 
 }
+let array = "Telefon: 24220956~E-Mail: mail@hcandersenfestspillerne.dk~ Adresse: Kanalvej 154C~ Odense C";
+
+var quizArray = array.split('~');
+var finalString = quizArray.join('<br/>');
+
+document.getElementById('foot1').innerHTML = finalString;
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 55.418969064295474, lng: 10.376959240657936 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("foot1"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
