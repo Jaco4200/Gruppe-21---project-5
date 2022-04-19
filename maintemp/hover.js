b60233cf315893ea8e47e6c750ae2df86c1f04ca
JETEXODUS <95678727+JETEXODUS@users.noreply.github.com>
2022-04-19 22:19:27 +0200

    Har opdateret billeder på min del af CSS'en og fundet ud af at lave flere hover kanpper

--- maintemp/hover.js
+++ maintemp/hover.js
@@ -7,3 +7,11 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
+function on1() {
+  document.getElementById("overlay1").style.display = "block";
+}
+
+function off1() {
+  document.getElementById("overlay1").style.display = "none";
+}
