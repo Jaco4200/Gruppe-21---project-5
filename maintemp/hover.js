7aa14ba77c5457cc9a406a3007ac0c20425ec0f8
Jaco4200 <95678568+Jaco4200@users.noreply.github.com>
2022-04-20 09:43:13 +0200

    Revert "Merge branch 'main' of https://github.com/Jaco4200/Gruppe-21---project-5"
    This reverts commit 2588c02560e747c93d0cf9ad30ee2a6abff1725a, reversing
    changes made to 701504fd594c397d6cadd3e30894106d0fcc74f0.

--- maintemp/hover.js
+++ maintemp/hover.js
@@ -15,3 +15,11 @@ function on1() {
 function off1() {
   document.getElementById("overlay1").style.display = "none";
 }
+
+function on2() {
+  document.getElementById("overlay2").style.display = "block";
+}
+
+function off2() {
+  document.getElementById("overlay2").style.display = "none";
+}
