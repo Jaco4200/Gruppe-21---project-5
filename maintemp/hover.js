a38086599b4de7fce5c830e2ee042c2c7412297f
JETEXODUS <95678727+JETEXODUS@users.noreply.github.com>
2022-04-20 10:17:04 +0200

    har lavet en ekstra nyheds sektion for at teste det

--- maintemp/hover.js
+++ maintemp/hover.js
@@ -7,3 +7,11 @@ function on() {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
+function on2() {
+  document.getElementById("overlay2").style.display = "block";
+}
+
+function off2() {
+  document.getElementById("overlay2").style.display = "none";
+}
