1ff7f23dbe2965acaffaba05fe331d04e38170cf
Jaco4200 <95678568+Jaco4200@users.noreply.github.com>
2022-04-20 10:02:17 +0200

    Update Style.css

--- maintemp/Javasc.js
+++ maintemp/Javasc.js
@@ -49,30 +49,3 @@ if (e.target !== dropdownBtn) {
     dropdownMenu.classList.add("hide");
 }
 });
-
-let slideIndex1 = 1;
-showSlides(slideIndex1);
-
-function plusSlides(n) {
-  showSlides(slideIndex1 += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex1 = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("ourSlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex1 = 1}
-  if (n < 1) {slideIndex1 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex1-1].style.display = "block";
-  dots[slideIndex1-1].className += " active";
-}
