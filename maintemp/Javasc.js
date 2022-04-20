a32fa9d19d26fa7e6314746b353e339cc4c7af7b
Jaco4200 <95678568+Jaco4200@users.noreply.github.com>
2022-04-20 09:38:45 +0200

    fix

--- maintemp/Javasc.js
+++ maintemp/Javasc.js
@@ -50,29 +50,29 @@ if (e.target !== dropdownBtn) {
 }
 });
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex1 = 1;
+showSlides(slideIndex1);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex1 += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideIndex1 = n);
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("ourSlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex1-1].style.display = "block";
+  dots[slideIndex1-1].className += " active";
 }
