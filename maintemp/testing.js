41b8ca8ebeac6b1cac779bb889b6149064393665
Jaco4200 <95678568+Jaco4200@users.noreply.github.com>
2022-04-21 10:28:27 +0200

    matching

--- maintemp/testing.js
+++ maintemp/testing.js
@@ -1,26 +1,26 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide1(n) {
+  showSlides1(slideIndex1 = n);
 }
 
-function showSlides(n) {
+function showSlides1(n) {
   let i;
-  let slides = document.getElementsByClassName("ourSlides");
+  let slides1 = document.getElementsByClassName("ourSlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  if (n > slides1.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides1.length}
+  for (i = 0; i < slides1.length; i++) {
+    slides1[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides1[slideIndex1-1].style.display = "block";
+  dots[slideIndex1-1].className += " active";
 }
