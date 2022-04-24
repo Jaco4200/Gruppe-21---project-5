838953fc20b76042bb1673f0cb91a5b50a2a0a32
JETEXODUS <95678727+JETEXODUS@users.noreply.github.com>
2022-04-24 10:55:42 +0200

    har lavet array på min js kode

--- maintemp/hover.js
+++ maintemp/hover.js
@@ -32,20 +32,32 @@ function off4() {
   document.getElementById("overlay4").style.display = "none";
 }
 
+let email = [];
+email [0] = "e-mail ikke korrekt";
+email [1] = "korrekt e-mail";
 
 
 function myFunction() {
   let at = document.getElementById("email").value.indexOf("@");
 
   if (at == -1) {
-    alert("e-mail ikke korrekt!");
+    alert(email [0]);
     submitOK = "false";
   }
   else {
-    alert("korrekt e-mail")
+    alert(email [1])
   }
 
   if (submitOK == "false") {
     return false;
   }
 }
+
+output.innerHTML+="<h1>Tilmeld dig nyhedsbrevet</h1>"
+email.sort();
+for (let i = 0; i < email.length; i++) {
+  output.innerHTML+= "<br>" + emailboks[i];
+}
+
+email.reverse();
+console.log(email);
